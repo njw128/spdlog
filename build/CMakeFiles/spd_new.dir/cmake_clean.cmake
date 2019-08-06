1bace0153f4ed7ddfc7d7ed17a5e46dd0b596b0f
jwniu <njw128@126.com>
2019-08-06 22:02:23 +0800

    basic version

---
+++ build/CMakeFiles/spd_new.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spd_new.dir/main.cpp.o"
+  "spd_new.pdb"
+  "spd_new"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spd_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
